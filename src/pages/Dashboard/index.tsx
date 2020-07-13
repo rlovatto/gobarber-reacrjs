ce6a1174a5e6e2bee1e89bd6017bb2b6b7fff4c2
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-13 20:08:13 +0100

    creating routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// FC stands for Function Component. It is used this way because the ease to define the type for the variable
+// instead of this way, could be used the function Dashboard () {} sintax
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>
+}
+
+export default Dashboard;
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+    // Switch assures that only one route is displayed at a time. Otherwise all the routes that match the query would be presented
+    <Switch>
+      {/* in the first route always put the keyword exact to assure, because react-router-dom consider path to include its value. So, without "exact" when you try to navigate to repository page, the systems would always send you to dashboad page */}
+
+        <Route path='/' exact component={Dashboard} />
+        <Route path='/repository' component={Repository} />
+
+    </Switch>
+)
+
+export default Routes;
