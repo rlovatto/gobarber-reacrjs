8434749f812c00005f5e6ff2542ea7db7fbeb19e
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-14 23:35:41 +0100

    Connecting to github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+
+  }
+}
+
+
 // FC stands for Function Component. It is used this way because the ease to define the type for the variable
 // instead of this way, could be used the function Dashboard () {} sintax
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo ] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer"/>
       <Title>Explore repositórios no Github</Title>
 
-      <Form >
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input value={newRepo}
+                onChange={e => setNewRepo(e.target.value)}
+                placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEaONMx6iXN4w/profile-displayphoto-shrink_400_400/0?e=1600300800&v=beta&t=BbgIx4B1QLCJT8tB_L7GEGVwwsKjx4gxwoJz5QlUnhs"
-                alt="Robinson Lovatto"/>
-          <div>
-            <strong>react-repo</strong>
-            <p>React repository for dev testing</p>
-          </div>
+        {
+          repositories.map(repository => (
+            <a key={repository.full_name} href="teste">
+              <img src={repository.owner.avatar_url}
+                    alt={repository.owner.login}/>
+              <div>
+                <strong>{repository.full_name}</strong>
+                <p>{repository.description}</p>
+              </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEaONMx6iXN4w/profile-displayphoto-shrink_400_400/0?e=1600300800&v=beta&t=BbgIx4B1QLCJT8tB_L7GEGVwwsKjx4gxwoJz5QlUnhs"
-                alt="Robinson Lovatto"/>
-          <div>
-            <strong>react-repo</strong>
-            <p>React repository for dev testing</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEaONMx6iXN4w/profile-displayphoto-shrink_400_400/0?e=1600300800&v=beta&t=BbgIx4B1QLCJT8tB_L7GEGVwwsKjx4gxwoJz5QlUnhs"
-                alt="Robinson Lovatto"/>
-          <div>
-            <strong>react-repo</strong>
-            <p>React repository for dev testing</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+              <FiChevronRight size={20} />
+            </a>
+          ))
+        }
       </Repositories>
     </>
   )
