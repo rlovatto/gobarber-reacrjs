44cd0404d098fa194a8c2b20564dc96278741bcc
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-15 19:37:37 +0100

    Dealing with errors, styling conditionally

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
   full_name: string;
@@ -20,17 +20,29 @@ interface Repository {
 // instead of this way, could be used the function Dashboard () {} sintax
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo ] = useState('');
+  const [inputError, setInputError ] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
 
-    const response = await api.get<Repository>(`repos/${newRepo}`);
+    if (!newRepo){
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
 
-    const repository = response.data;
+    try{
+      const response = await api.get<Repository>(`repos/${newRepo}`);
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+
+    } catch(err) {
+      setInputError('Erro na busca por esse repositório.');
+    }
 
   }
 
@@ -39,13 +51,17 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer"/>
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      {/* the operator !! means truthy and falsy values. is the same as Boolean(inputError) */}
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input value={newRepo}
                 onChange={e => setNewRepo(e.target.value)}
                 placeholder="Digite o nome do repositório"
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {/* in the line below the component is only displayed when the first condition is true */}
+      { inputError && <Error> {inputError} </Error>}
 
       <Repositories>
         {
