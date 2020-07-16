257b781a99833aee78e92cfe4937d713544c0c3b
Robinson Lovatto <robinsonrlovatto@hotmail.com>
2020-07-16 09:13:03 +0100

    Styling repositories detail

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,65 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
 }
 
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>();
+  // const { params } = useRouteMatch<RepositoryParams>();
 
-return <h1>Repository:{params.repository}</h1>
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer"/>
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEaONMx6iXN4w/profile-displayphoto-shrink_400_400/0?e=1600300800&v=beta&t=BbgIx4B1QLCJT8tB_L7GEGVwwsKjx4gxwoJz5QlUnhs" alt="rocketseat"/>
+          <div>
+            <strong>rockeatseat</strong>
+            <p>descrição do repo</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+
+      </RepositoryInfo>
+
+      <Issues>
+        <Link  to={`qwqwwqw`}>
+          <div>
+            <strong>231312313</strong>
+            <p>dsdfsfsfsf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 }
 
 export default Repository;
---
+++ src/pages/Repository/styles.ts
@@ -0,0 +1,126 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #a8a8b3;
+    transition: color 0.2s;
+
+    &:hover {
+      color: #666;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
+
+  }
+`;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #737380;
+        margin-top: 4px;
+      }
+    }
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        color: #3d3d4d;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #6c6c80;
+      }
+    }
+  }
+`;
+
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    /* only aplied when this element is followed by another a */
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p{
+        font-size: 18px;
+        color: #A8A8B3;
+        margin-top: 4px;
+      }
+    }
+
+    /* margin left auto take all the available space from the left to the right  */
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
